ad1cadf65eadaaba125175dd4d4d100677e2da6f
Sam Walker <2379093w@student.gla.ac.uk>
2020-11-16 19:02:13 +0000

    added a basic directory issue#44

---
+++ vilosky_paths/vilosky_paths/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for vilosky_paths project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vilosky_paths.settings')
+
+application = get_wsgi_application()
