7444c4934120ad9f45706e00bed82d940672833e
Paul <2393709o@student.gla.ac.uk>
2020-11-18 15:42:38 +0000

    Created basic  views and associated url paths

--- vilosky_paths/vilosky_paths/urls.py
+++ vilosky_paths/vilosky_paths/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from paths import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('', views.home, name='home'),
+	path('paths/', include('paths.urls')),
+	#the above maps any URLs starting with paths/ to be handled by the paths app
+	path('admin/', admin.site.urls),
 ]
