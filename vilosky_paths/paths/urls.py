ae9fe1bb92e3f3162dd9f888417e85ecffea0a5e
Jaroslav <2394122h@student.gla.ac.uk>
2020-12-19 11:48:24 +0000

    Add login functionality with no external modules [Issue #61]
    *Add a custom UserProfile model
    *Add a corresponding form
    *Add a view for registering
    *Add a view for logging in
    *Add a view for logging out
    *Add a basic template for registering
    *Add a basic template for logging in
    *Register the new urls
    *Update links in the base template

--- vilosky_paths/paths/urls.py
+++ vilosky_paths/paths/urls.py
@@ -6,5 +6,8 @@ app_name = 'paths'
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- vilosky_paths/paths/views.py
+++ vilosky_paths/paths/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from paths.forms import UserForm, UserProfileForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,3 +16,50 @@ def search(request):
 def dashboard(request):
 	return render(request, 'paths/dashboard.html')
 
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+        
+    return render(request, 'paths/register.html', context = {'user_form':user_form,
+                                                            'profile_form':profile_form,
+                                                            'registered':registered})
+                                                            
+                                                            
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('paths:home'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'paths/login.html')
+        
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('paths:home'))
